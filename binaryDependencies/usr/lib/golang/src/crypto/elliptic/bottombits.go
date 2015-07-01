11452812493b5178f7f59deb1051a0c7a45603aa
Adam Kurkiewicz <adam@kurkiewicz.pl>
2015-07-01 22:15:52 +0100

    go 1.4 without rpms and redis server

---
+++ binaryDependencies/usr/lib/golang/src/crypto/elliptic/bottombits.go
@@ -0,0 +1,14 @@
+
+// Copyright 2012 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package elliptic
+
+const bottom12Bits = 0xfff
+const bottom28Bits = 0xfffffff
+
+const two31p3 = 1<<31 + 1<<3
+const two31m3 = 1<<31 - 1<<3
+const two31m15m3 = 1<<31 - 1<<15 - 1<<3
+
