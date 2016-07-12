73929c9aea484107d4e0cd07c449e8cf9784afe9
Adam Kurkiewicz <adam@kurkiewicz.pl>
2016-07-13 00:59:30 +0100

    Simple CLI

--- boardLib/main/mainAdam.go
+++ boardLib/main/mainAdam.go
@@ -18,9 +18,62 @@ package main
 
 import "../../boardLib"
 import "fmt"
+import "bufio"
+import "os"
+import "log"
+import "strings"
+import "encoding/json"
+import "../../API2048"
+
+func prettyPrint(data string, oldBoard bool){
+	accessString := "NewBoard"
+	if oldBoard{
+		accessString = "OldBoard"
+	}
+	var result map[string]interface{}
+	_ = json.Unmarshal([]byte(data), &result)
+	parsedResult := result[accessString].([]interface{})
+	for i := 0; i < API2048.BoardSize; i++{
+		parsedResultInner := parsedResult[i].([]interface{})
+		for j := 0; j < API2048.BoardSize; j++ {
+			fmt.Print(parsedResultInner[j], "\t")
+		}
+		fmt.Println("")
+	}
+	fmt.Println("Score ", result["RoundNo"])
+	return
+}
 
 func main() {
 	move := boardLib.CreateMove()
 	move.InitFirstMove()
-	fmt.Println(move.InternalView())
+	prettyPrint(move.ExternalView(), true)
+	var direction string
+	var line string
+	var err error
+	for !move.GetGameOver() {
+		fmt.Println("press h, j, k or l and press enter.")
+		reader := bufio.NewReader(os.Stdin)
+		line, err = reader.ReadString('\n')
+		if err != nil {
+			log.Println("Error reading from stdin")
+			os.Exit(1)
+		}
+		switch {
+			case strings.Contains(line, "h"):
+				direction = "left"
+			case strings.Contains(line, "j"):
+				direction = "down"
+			case strings.Contains(line, "k"):
+				direction = "up"
+			case strings.Contains(line, "l"):
+				direction = "right"
+			default:
+				fmt.Println("")
+		}
+		move.SetDirection(direction)
+		move.ResolveMove()
+		prettyPrint(move.ExternalView(), false)
+		move = move.CreateNextMove()
+	}
 }
